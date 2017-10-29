5f7d0121670c133b3a3a37a386c87a51852371a0
natasha <natasha.frumkin@gmail.com>
2017-10-29 02:40:54 -0400

    sniffing packets file added

--- scratch.py
+++ scratch.py
@@ -1,15 +1,4 @@
-from datasketch import MinHash
 from scapy.all import *
-import os
 
-# parse pcap into an object
-pcap_obj = rdpcap('/media/nvidia/windows/normal-traffic/' + os.listdir('/media/nvidia/windows/normal-traffic/')[2])
-
-# hash for current pcap file
-pcap_hash = MinHash()
-
-# create the hash object
-for packet in pcap_obj:
-	pcap_hash.update(str(packet).encode("utf8"))
-
-print(len(pcap_hash.hashvalues))
+packets = sniff(count=5)
+wrpcap('sniffsniff.pcap', packets)
